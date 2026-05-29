aca96dd51b76db0ba4ff018c79cbc7fc2846e32d
ify1237ibiam <ifegwuifeanyi2021@gmail.com>
2026-05-29 06:50:14 +0100

    AAll done

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,6 +5,7 @@ const { protect } = require("../middleware/authMiddleware");
 
 router.post("/signup", authController.signup);
 router.post("/login", authController.login);
+router.post("/switch-token", authController.login); // ← add this line only
 router.get("/me", protect, authController.getMe);
 router.get("/refresh", protect, authController.refreshUser);
 router.get("/stats", protect, authController.getUserStats);
